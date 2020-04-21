325a24030acd73c1f8727eec9795b2088e550a08
lemuelZara <lemuel_zara@outlook.com>
2020-04-21 16:27:06 -0300

    Array.prototype.meuFilter

---
+++ module-02/filter.js
@@ -0,0 +1,33 @@
+const { obterPessoas } = require('./service')
+
+Array.prototype.meuFilter = function (callback) {
+    const novaLista = []
+
+    // O this é o Array que estou passando
+    for (const [index, value] of this.entries()) {
+        console.log('Item: ', index)
+        const resultado = callback(value, index, this)
+
+        if (!resultado) {
+            continue
+        } else {
+            novaLista.push(value)
+        }
+    }
+
+    return novaLista
+}
+
+async function main() {
+    try {
+        const { results } = await obterPessoas('a')
+
+        const familiaLars = results.meuFilter(pessoa => pessoa.name.toLowerCase().indexOf('lars') !== -1)
+
+        console.log(familiaLars.map(item => item.name))
+    } catch (error) {
+        console.error('Erro:', error)
+    }
+}
+
+main()
