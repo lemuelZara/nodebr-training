0da8ccda8ccd0d1ced5e83dceecde7a441818835
lemuelZara <lemuel_zara@outlook.com>
2020-04-22 14:29:39 -0300

    manipulating files - delete

--- module-04/__tests__/index.test.js
+++ module-04/__tests__/index.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 
-const { listarHerois } = require('../database')
+const { listarHerois, cadastrarHeroi, removerHeroi } = require('../database')
 
 const DEFAULT_ITEM_CADASTRAR = {
     id: 1,
@@ -9,11 +9,32 @@ const DEFAULT_ITEM_CADASTRAR = {
 }
 
 describe('Suíte de Manipulação de Heróis', () => {
-    it('deve cadastrar um herói usando arquivos', async () => {
+    before(async () => {
+        await cadastrarHeroi(DEFAULT_ITEM_CADASTRAR)
+    })
+
+    it('deve pesquisar um herói usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
 
         // Pega a 1ª posição do Array
         const [resultado] = await listarHerois(expected.id)
+
+        assert.deepEqual(resultado, expected)
+    })
+
+    it('deve cadastrar um herói usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+
+        // Pega a 1ª posição do Array
+        const resultado = await cadastrarHeroi(expected)
+        const [atual] = await listarHerois(expected.id)
+
+        assert.deepEqual(atual, expected)
+    })
+
+    it('deve remover um herói por ID', async () => {
+        const expected = true
+        const resultado = await removerHeroi(DEFAULT_ITEM_CADASTRAR.id)
 
         assert.deepEqual(resultado, expected)
     })
--- module-04/database.js
+++ module-04/database.js
@@ -1,4 +1,5 @@
-const { readFileSync } = require('fs')
+const { readFileSync, writeFileSync } = require('fs')
+const path = require('path')
 const { promisify } = require('util')
 
 const listarHerois = async id => {
@@ -9,18 +10,58 @@ const listarHerois = async id => {
     return dadosFiltrados
 }
 
+// Leio os dados dentro do arquivo herois.json
 const obterDadosHerois = () => {
-    const arquivo = readFileSync('./herois.json', 'utf8')
+    const arquivo = readFileSync(path.resolve(__dirname, './herois.json'))
 
     return JSON.parse(arquivo.toString())
 }
 
-const escreverHerois = () => {
+const escreverHerois = dados => {
+    writeFileSync(path.resolve(__dirname, './herois.json'), JSON.stringify(dados))
 
+    return true
+}
+
+const cadastrarHeroi = async heroi => {
+    const dados = await obterDadosHerois()
+
+    const id = heroi.id <= 2 ? heroi.id : Date.now()
+
+    const heroiComId = {
+        id,
+        ...heroi
+    }
+
+    const resultado = escreverHerois([...dados, heroiComId])
+
+    return resultado
+}
+
+const removerHeroi = async id => {
+    if (!id) {
+        return escreverHerois([])
+    }
+
+    const dados = await obterDadosHerois()
+
+    // Retorna o índice do herói que quero remover
+    const index = dados.findIndex(item => item.id === parseInt(id))
+
+    if (index === 1) {
+        throw Error('O Herói não existe!')
+    }
+
+    // Remove todos os heróis com aquele índice
+    dados.splice(index)
+
+    return escreverHerois(dados)
 }
 
 module.exports = {
     listarHerois,
     obterDadosHerois,
-    escreverHerois
+    escreverHerois,
+    cadastrarHeroi,
+    removerHeroi
 }
